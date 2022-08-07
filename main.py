d303baab1267c19e9b0d5fce14d1bc8c508268b1
Raxath <36684140+Raxath@users.noreply.github.com>
2022-08-07 19:41:50 -0400

    Changed from every 24 hours to once per day at a specific hour

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ from os import environ
 url_vividseat = 'https://www.vividseats.com/itzy-tickets-rosemont-rosemont-theatre-11-7-2022--concerts-k-pop/production/4000042?AID=Performer-MainProductionList'
 file_vividseat = "vividseat.html"
 excel_headers = ["price", "section type", "section number", "Row", "tickets avaliable"]
-delay = 86400
+delay = 3000 
+target_time = 3
+current_time = datetime.now()
 email = environ.get("SCRAPE_RECEVING_EMAIL")
 load_dotenv()
 #------------------Methods------------------#
@@ -101,44 +103,45 @@ def dump_vividseats(save_file, excel_headers):
 
 #------------------Project Logic------------------#
 while True:
-    #############scrape vividseat data
-    scrape(url_vividseat, file_vividseat)
-    #############
-    
-    ##############Update Vividseat excel
-    df_vividseat = dump_vividseats(file_vividseat, excel_headers)
-    
-    # "ï¿½" appears in the data where "-" should be this fixes that.
-    df_vividseat["tickets avaliable"] = df_vividseat["tickets avaliable"].replace("ï¿½", "-", regex=True)
-    
-    df_vividseat.to_excel("vividseat.xlsx", index=False)
-    ##############
+    if current_time.hour == target_time:
+        #############scrape vividseat data
+        scrape(url_vividseat, file_vividseat)
+        #############
+        
+        ##############Update Vividseat excel
+        df_vividseat = dump_vividseats(file_vividseat, excel_headers)
+        
+        # "ï¿½" appears in the data where "-" should be this fixes that.
+        df_vividseat["tickets avaliable"] = df_vividseat["tickets avaliable"].replace("ï¿½", "-", regex=True)
+        
+        df_vividseat.to_excel("vividseat.xlsx", index=False)
+        ##############
 
-    #############Create The Email
-    # Setup nylas api connection
-    nylas = APIClient(
-                      client_id=environ.get("NYLAS_CLIENT_ID"),
-                      client_secret=environ.get("NYLAS_CLIENT_SECRET"),
-                      access_token=environ.get("NYLAS_ACCESS_TOKEN")
-                    )
-    # Create your attachment
-    attachment = open("vividseat.xlsx", "rb")
-    attach = nylas.files.create()
-    attach.filename = 'vividseat.xlsx'
-    attach.stream = attachment
-    
-    # Draft your email and attach your file
-    draft = nylas.drafts.create()
-    draft.subject = "Ticket price update!"
-    draft.body = "Hi Baby! heres your ticket prices for the day! They currently reflect the prices seen at Vividseat. Let me know how I can improve this!"
-    draft.to = [{"name": "Mai Baby <3", "email": email}]
-    draft.attach(attach)
-    
-    # Send your email!
-    draft.send()
-    attachment.close()
-    
-    # notify the task was complete and wait to do it again.
-    now = datetime.now()
-    print(f"Task compete its's {now}")
+        #############Create The Email
+        # Setup nylas api connection
+        nylas = APIClient(
+                        client_id=environ.get("NYLAS_CLIENT_ID"),
+                        client_secret=environ.get("NYLAS_CLIENT_SECRET"),
+                        access_token=environ.get("NYLAS_ACCESS_TOKEN")
+                        )
+        # Create your attachment
+        attachment = open("vividseat.xlsx", "rb")
+        attach = nylas.files.create()
+        attach.filename = 'vividseat.xlsx'
+        attach.stream = attachment
+        
+        # Draft your email and attach your file
+        draft = nylas.drafts.create()
+        draft.subject = "Ticket price update!"
+        draft.body = "Hi Baby! heres your ticket prices for the day! They currently reflect the prices seen at Vividseat. Let me know how I can improve this!"
+        draft.to = [{"name": "Mai Baby <3", "email": email}]
+        draft.attach(attach)
+        
+        # Send your email!
+        draft.send()
+        attachment.close()
+        
+        print("Xlsx Sent")
+    else:
+        print("Target time not met")
     time.sleep(delay)
