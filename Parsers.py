048ac434945b9e7bf4ee8a2a882ff76a509c4994
Raxath <36684140+Raxath@users.noreply.github.com>
2022-08-08 22:32:06 -0400

    Made Parsers and ScrameManager into classes

---
+++ Parsers.py
@@ -0,0 +1,17 @@
+from turtle import delay
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import re
+from nylas import APIClient
+import time
+from datetime import datetime
+from dotenv import load_dotenv
+from os import environ
+import ScrapeManager as sm
+
+class Parsers:
+    def test(self):
+        pass
--- ScrapeManager.py
+++ ScrapeManager.py
@@ -5,80 +5,81 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import re
 import time
+class ScrapeManager:
 #------------------Methods------------------#
-def scrape(url, file):
-    # setup the options for our webDriver
-    options = Options()
-    options.headless = False
-    options.add_argument("--window-size=1920,1200")
+    def scrape(self, url, file):
+        # setup the options for our webDriver
+        options = Options()
+        options.headless = False
+        options.add_argument("--window-size=1920,1200")
 
-    # installs and runs the Chrome Driver
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-    driver.get(url)
-    time.sleep(10)
-    html = driver.page_source
-    driver.quit()   
-    
-    # saves the scrapped HTML to my save file
-     
-    save_file = open(file, "w")
-    save_file.write(html)
-
-# loads HTML from file into BeautifulSoup 
-def load_soup_from_file(file):
-    html = open(file, "r").read()
-    return BeautifulSoup(html, features="html.parser")
-
-# get list of divs to parse
-def get_divs(soup):
-    all_divs = soup.find_all("div", "styles_listingRowContainer__3hXZy")
-    dirty_data = []
-    for div in all_divs:
-        words = div.text.split()
-        dirty_data.append(words)
-    return dirty_data
-
-# Get data dump from vividseats
-# TODO Make this Method Generic
-def dump_vividseats(save_file, excel_headers):
-    # load HTML into Beautiful soup
-    soup = load_soup_from_file(save_file)
-    
-    # clean data that will be returned at the end
-    df_clean_data = pd.DataFrame(columns=excel_headers)
-    
-    # find the Divs with the pricing info and build rows for a panda data frame
-    all_divs = get_divs(soup)  
-    for div in all_divs:
-        output = {}
-        output["price"] = div[0]
-        output["section type"] = div[1][2:]
-        output["section number"] = div[2]
+        # installs and runs the Chrome Driver
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+        driver.get(url)
+        time.sleep(10)
+        html = driver.page_source
+        driver.quit()   
         
-        # the row and ticket data were mixed into one field when importing
-        # this was the solution
-        row_tickets = div[5]
+        # saves the scrapped HTML to my save file
         
-            # what happens when string starts with a letter
-            # this happens when the seat is not a pit seat
-        if re.search("^[a-zA-Z]", row_tickets) is not None:
+        save_file = open(file, "w")
+        save_file.write(html)
+
+    # loads HTML from file into BeautifulSoup 
+    def load_soup_from_file(self, file):
+        html = open(file, "r").read()
+        return BeautifulSoup(html, features="html.parser")
+
+    # get list of divs to parse
+    def get_divs(self, soup):
+        all_divs = soup.find_all("div", "styles_listingRowContainer__3hXZy")
+        dirty_data = []
+        for div in all_divs:
+            words = div.text.split()
+            dirty_data.append(words)
+        return dirty_data
+
+    # Get data dump from vividseats
+    # TODO Make this Method Generic
+    def dump_vividseats(self, save_file, excel_headers):
+        # load HTML into Beautiful soup
+        soup = self.load_soup_from_file(save_file)
+        
+        # clean data that will be returned at the end
+        df_clean_data = pd.DataFrame(columns=excel_headers)
+        
+        # find the Divs with the pricing info and build rows for a panda data frame
+        all_divs = self.get_divs(soup)  
+        for div in all_divs:
+            output = {}
+            output["price"] = div[0]
+            output["section type"] = div[1][2:]
+            output["section number"] = div[2]
             
-            # loop over row and ticket avaliable data to find first number
-            # then set the row and tickets avaliable values
-            # TODO FIX THIS SHIT and add logs
-            i = 0
-            for current_char in row_tickets:
-                if current_char.isnumeric():
-                    output["Row"] = row_tickets[0:i]
-                    output["tickets avaliable"] = row_tickets[i:]
-                    break
-                i = i + 1
+            # the row and ticket data were mixed into one field when importing
+            # this was the solution
+            row_tickets = div[5]
+            
+                # what happens when string starts with a letter
+                # this happens when the seat is not a pit seat
+            if re.search("^[a-zA-Z]", row_tickets) is not None:
                 
-            # what happens when string starts with a number
-        else:
-            output["Row"] = row_tickets[0:1]
-            output["tickets avaliable"] = row_tickets[1:]
-        
-        df_to_append = pd.DataFrame(output, index=[0])
-        df_clean_data = pd.concat([df_clean_data, df_to_append])  
-    return df_clean_data
+                # loop over row and ticket avaliable data to find first number
+                # then set the row and tickets avaliable values
+                # TODO FIX THIS SHIT and add logs
+                i = 0
+                for current_char in row_tickets:
+                    if current_char.isnumeric():
+                        output["Row"] = row_tickets[0:i]
+                        output["tickets avaliable"] = row_tickets[i:]
+                        break
+                    i = i + 1
+                    
+                # what happens when string starts with a number
+            else:
+                output["Row"] = row_tickets[0:1]
+                output["tickets avaliable"] = row_tickets[1:]
+            
+            df_to_append = pd.DataFrame(output, index=[0])
+            df_clean_data = pd.concat([df_clean_data, df_to_append])  
+        return df_clean_data
