c456d90b1e991668f702c450ed57d17ae0cb01da
Raxath <36684140+Raxath@users.noreply.github.com>
2022-08-08 22:27:07 -0400

    removed unneeded imports

--- ScrapeManager.py
+++ ScrapeManager.py
@@ -1,15 +1,10 @@
-from turtle import delay
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import re
-from nylas import APIClient
 import time
-from datetime import datetime
-from dotenv import load_dotenv
-from os import environ
 #------------------Methods------------------#
 def scrape(url, file):
     # setup the options for our webDriver
